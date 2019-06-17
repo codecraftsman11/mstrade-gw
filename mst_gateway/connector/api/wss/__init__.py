9d1b053e17d1f771611cf1256b7afe2362671b0e
Vladimir Belinsky <belka158@gmail.com>
2019-06-17 12:42:24 +0300

    Add subscribe/unsubscribe method to wss

--- mst_gateway/connector/api/wss/__init__.py
+++ mst_gateway/connector/api/wss/__init__.py
@@ -1,9 +1,13 @@
-from typing import Dict
+from typing import Dict, Set
 from abc import ABCMeta, abstractmethod
 from logging import Logger
 from ...base import Connector
 from .. import ERROR_OK
 from .channel import StockWssChannel
+
+
+Route = str
+SubscriptionRoutes = Dict[Route, Set[StockWssChannel]]
 
 
 class StockWssConnector(Connector):
@@ -13,13 +17,36 @@ class StockWssConnector(Connector):
     def __init__(self, url: str = None, auth: dict = None, logger: Logger = None):
         self._url: str = url if url is not None else self.__class__.BASE_URL
         self._error: tuple = ERROR_OK
-        self._channels: Dict[str, StockWssChannel]
+        self._subscriptions: Dict[str, SubscriptionRoutes]
         super().__init__(auth, logger)
 
-    @abstractmethod
-    def subscirbe(self, channel: str, key: str, wss_channel: StockWssChannel):
+    def subscirbe(self, subscription: str, wss_channel: StockWssChannel, route: str = ""):
+        if not route:
+            self._subscribe_root(subscription, wss_channel)
+            return
+        channelset = self._make_channelset(subscription, route)
+        channelset.add(wss_channel)
+
+    def _subscribe_root(self, subscription: str, wss_channel: StockWssChannel):
         pass
 
+    def _make_channelset(self, subscription, route):
+        # pylint: disable=no-self-use,unused-argument
+        """
+        Create channel set
+        """
+        return set()
+
     @abstractmethod
-    def unsubscribe(self, channel: str, key: str):
-        pass
+    def unsubscribe(self, wss_channel: StockWssChannel, subscription: str,
+                    route: str = ""):
+        channelset = self._get_channelset(subscription, route)
+        if channelset:
+            channelset.remove(wss_channel)
+
+    def _get_channelset(self, subscription: str, route: str):
+        if subscription not in self._subscriptions:
+            return None
+        if route not in self._subscriptions[subscription]:
+            return None
+        return self._subscriptions[subscription][route]
