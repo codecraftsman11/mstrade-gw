b3724d546d4b32824bad2555ae34db2fabb2c6d4
Vladimir Belinsky <belka158@gmail.com>
2020-02-23 22:33:10 +0200

    Add order ttl and execution field types

--- mst_gateway/connector/api/stocks/bitmex/var.py
+++ mst_gateway/connector/api/stocks/bitmex/var.py
@@ -6,10 +6,12 @@ BITMEX_MAX_QUOTE_BINS_COUNT = 750
 ORDER_TYPE_WRITE_MAP = {
     api.OrderType.market: 'Market',
     api.OrderType.limit: 'Limit',
-    api.OrderType.sl_market: 'Stop',
-    api.OrderType.sl_limit: 'StopLimit',
-    api.OrderType.tp_market: 'MarketIfTouched',
-    api.OrderType.tp_limit: 'LimitIfTouched'
+}
+
+
+ORDER_CLOSE_TYPE_MAP = {
+    api.OrderType.stop_loss: 'Stop',
+    api.OrderType.take_profit: 'MarketIfTouched',
 }
 
 
