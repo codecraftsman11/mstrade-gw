9e0f9763bcfa407b86864c3739a372f12b8b1d62
A <foo@bar.com>
2020-06-11 18:42:36 +0300

    add state to position data

--- mst_gateway/connector/api/stocks/bitmex/wss/serializers/position.py
+++ mst_gateway/connector/api/stocks/bitmex/wss/serializers/position.py
@@ -8,6 +8,12 @@ class BitmexPositionSerializer(BitmexSerializer):
         return True
 
     def _load_data(self, message: dict, item: dict) -> dict:
+        state = self._get_state(item.get('symbol'))
+        if not item.get('avgEntryPrice'):
+            item['avgEntryPrice'] = state[0]['entry_price']
+        if not item.get('liquidationPrice'):
+            item['liquidationPrice'] = state[0]['liquidation_price']
+
         data = dict(
                 timestamp=item.get('timestamp'),
                 symbol=item.get('symbol'),
