1d4fd8f6222e895a2cda6dced3a5d14fc64e1e03
A <foo@bar.com>
2020-07-13 18:58:25 +0300

    fix symbol subscription

--- mst_gateway/connector/api/stocks/binance/wss/serializers/symbol.py
+++ mst_gateway/connector/api/stocks/binance/wss/serializers/symbol.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from typing import Union
 from .base import BinanceSerializer
 from ..utils import load_symbol_ws_data
 
@@ -6,7 +7,7 @@ from ..utils import load_symbol_ws_data
 class BinanceSymbolSerializer(BinanceSerializer):
     subscription = "symbol"
 
-    def is_item_valid(self, message: (dict, list), item) -> bool:
+    def is_item_valid(self, message: Union[dict, list], item) -> bool:
         if isinstance(message, list):
             return message[0].get('e') == "24hrTicker"
         elif isinstance(message, dict):
@@ -14,6 +15,6 @@ class BinanceSymbolSerializer(BinanceSerializer):
         return False
 
     def _load_data(self, message: dict, item: dict = None) -> list:
-        if isinstance(message, list):
+        if isinstance(item, list):
             return [load_symbol_ws_data(itm) for itm in item]
         return [load_symbol_ws_data(item)]
