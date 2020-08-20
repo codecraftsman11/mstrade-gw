b92bc77a042250f55a57c84c57076079d702d099
Mykola Sluchuk <mykola.sluchuk@gmail.com>
2020-08-20 18:07:57 +0300

    fixed bitmex wallet summary

--- mst_gateway/connector/api/stocks/bitmex/utils.py
+++ mst_gateway/connector/api/stocks/bitmex/utils.py
@@ -237,12 +237,13 @@ def load_wallet_summary(currencies: dict, balances: list, asset: str,
     total_balance = dict()
     for f in fields:
         total_balance[f] = 0
-    _asset_price = (currencies.get(f"{asset}usd".lower()) or 1)
     for b in balances:
         if b['currency'].lower() == asset.lower() or b['currency'].lower() == 'usd':
             _price = 1
+            _asset_price = 1
         else:
             _price = currencies.get(f"{b['currency']}usd".lower()) or 0
+            _asset_price = currencies.get(f"{asset}usd".lower()) or 1
         for f in fields:
             total_balance[f] += _price * (b[f] or 0) / _asset_price
     return total_balance
