0170932bc15f040c9922fd4bd648dc431e326c29
Alex L <magdee@mir.net.ua>
2020-09-24 14:52:28 +0000

    update pipeline

--- pipeline/proj_merge.sh
+++ pipeline/proj_merge.sh
@@ -7,8 +7,10 @@ scmd="'pip install -q -r requirements.txt'"
  && swbranch="'git checkout -b ${br_with_pq}'" \
  || swbranch="'git checkout ${br_with_pq}'"
 
+bb_token=${bb_token:-$(getToken)}
 pr=$(curl -s -X GET -H "Content-Type: application/json" -H "Authorization: Bearer ${bb_token}" "$BB_API/${BITBUCKET_REPO_FULL_NAME}/pullrequests?state=OPEN&pagelen=50" | jq -r ".values[] | select( .title | test(\"^\\\s*wip:\"; \"ix\") | not or ${wip_for_merge} ) | (\"git pull --no-edit origin \\(.source.branch.name)\" | @sh)")
 
+echo $?
 [[ -z $pr ]] && exit 1
 
 declare -a prarray="(${swbranch} ${pr} ${scmd})"
