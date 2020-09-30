5410dd2cda7bd34b7cc436516e094ea32e5079d9
Alex L <magdee@mir.net.ua>
2020-09-30 10:43:06 +0000

    update pipeline

--- pipeline/proj_dep_redeploy.sh
+++ pipeline/proj_dep_redeploy.sh
@@ -1,12 +1,14 @@
 echo "------------------------ [ Dependant ReDeploy ]" 
-deploy_launch=${DEPLOY_LAUNCH:-0}
-bb_token=${bb_token:-$(getToken)}
-redeployrepo="${BB_API}/${BITBUCKET_REPO_OWNER}/mstrade-api"
+redeploy_launch=${REDEPLOY_LAUNCH:-0}
+projectrepo="${BB_API}/${BITBUCKET_REPO_OWNER}/mstrade-api"
 
 declare -a cmds=()
-
-[[ $deploy_launch -eq 1 ]] && cmds+=("curl -s -X POST -H \"Content-Type: application/json\" -H \"Authorization: Bearer ${bb_token}\" --data '{\"target\": {\"type\": \"pipeline_pullrequest_target\"},\"selector\": {\"type\": \"pull-requests\",\"pattern\": \"**\"}}' \"${redeployrepo}/pipelines/\"")
-
+if [ $redeploy_launch -eq 1 ]; then
+ bb_token=${bb_token:-$(getToken)}
+ for repo in ${REDEPLOY_REPOS[@]}; do 
+  cmds+=("curl -s -X POST -H \"Content-Type: application/json\" -H \"Authorization: Bearer ${bb_token}\" --data '{\"target\":{\"type\":\"pipeline_ref_target\",\"ref_type\":\"branch\",\"ref_name\":\"master\",\"selector\":{\"type\":\"pull-requests\",\"pattern\":\"**\"}}}' \"${projectrepo}/${repo}/pipelines/\"")
+ done 
+fi
 runCommands "${cmds[@]}"
 unset cmds
 #-----
