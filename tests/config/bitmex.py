806eea8b85f64fea7f4816c0b324a31c9f7a763d
Mykola Sluchuk <mykola.sluchuk@gmail.com>
2020-09-30 17:47:46 +0300

    fixed config

--- tests/config/bitmex.py
+++ tests/config/bitmex.py
@@ -1,14 +1,13 @@
 from os import getenv
 
 
-BITMEX_NAME="bitmex"
-BITMEX_SCHEMA="margin1"
-BITMEX_SYMBOL="XBTUSD"
-BITMEX_URL="https://testnet.bitmex.com/api/v1"
-BITMEX_WSS_URL="wss://testnet.bitmex.com/realtime"
-BITMEX_API_KEY='ORqVaoVf1TJrVnKexpWjHfjk'
-BITMEX_API_SECRET='mvK7p-zYF5He2eistXxXUvASoJWRGvp6eOO5TF2gn4BHI2iB'
-BITMEX_EMAIL='devlifeoptional@tutamail.com'
-BITMEX_USERNAME='devlifeoptional@tutamail.com'
-BITMEX_LOCALE="en-US"
-
+BITMEX_NAME = "bitmex"
+BITMEX_SCHEMA = "margin1"
+BITMEX_SYMBOL = "XBTUSD"
+BITMEX_URL = "https://testnet.bitmex.com/api/v1"
+BITMEX_WSS_URL = "wss://testnet.bitmex.com/realtime"
+BITMEX_API_KEY = getenv('BITMEX_API_KEY')
+BITMEX_API_SECRET = getenv('BITMEX_API_SECRET')
+BITMEX_USERNAME = getenv('BITMEX_USERNAME', "belka158")
+BITMEX_EMAIL = getenv('BITMEX_EMAIL', "belka158@gmail.com")
+BITMEX_LOCALE = "en-US"
