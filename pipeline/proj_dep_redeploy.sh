bcfe3e8dea420385d896b9fb8c9342b3ac814214
Alex L <magdee@mir.net.ua>
2021-02-22 17:47:50 +0000

    update pipeline

--- pipeline/proj_dep_redeploy.sh
+++ pipeline/proj_dep_redeploy.sh
@@ -1,8 +1,17 @@
 echo "------------------------ [ Dependant ReDeploy ]" 
 redeploy_launch=${REDEPLOY_LAUNCH:-0}
 projectrepo="${BB_API}/${BITBUCKET_REPO_OWNER}"
+gittag=$(git describe --always --abbrev --tags --long)
+createdate=$(date +%Y%m%d%H%M%S)
+destfolder="${BITBUCKET_REPO_SLUG}---${gittag}-${createdate}"
 
+cd ..
 declare -a cmds=()
+cmds+=("mkdir -p ${destfolder}"
+       "cp -r ${BITBUCKET_REPO_SLUG}/mst_gateway ./${destfolder}/" 
+       "ln -sfn $(pwd)/$destfolder ${HOME}/${BITBUCKET_REPO_SLUG}-${BITBUCKET_DEPLOYMENT_ENVIRONMENT}"
+      )
+
 if [ $redeploy_launch -eq 1 ]; then
  bb_token=${bb_token:-$(getToken)}
  for repo in ${REDEPLOY_REPOS[@]}; do 
