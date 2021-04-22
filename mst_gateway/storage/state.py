22028f634102c27174e8d8482ca386719207f2bf
Vitalii Myrhorodskyi <vmyrhorodskyi@gmail.com>
2021-04-22 14:27:12 +0300

    pass timeout param

--- mst_gateway/storage/state.py
+++ mst_gateway/storage/state.py
@@ -3,7 +3,7 @@ from .base import BaseAsyncStorage, BaseSyncStorage
 
 class StateStorage(BaseSyncStorage):
     def set(self, key, value, timeout=None) -> None:
-        super().set(key=key, value=value)
+        super().set(key=key, value=value, timeout=timeout)
 
     def get(self, key, exchange: str = None, schema: str = None) -> dict:
         result = super().get(key=key, exchange=exchange, schema=schema)
@@ -12,7 +12,7 @@ class StateStorage(BaseSyncStorage):
 
 class AsyncStateStorage(BaseAsyncStorage):
     async def set(self, key, value, timeout=None) -> None:
-        await super().set(key=key, value=value)
+        await super().set(key=key, value=value, timeout=timeout)
 
     async def get(self, key, exchange: str = None, schema: str = None) -> dict:
         result = await super().get(key=key, exchange=exchange, schema=schema)
