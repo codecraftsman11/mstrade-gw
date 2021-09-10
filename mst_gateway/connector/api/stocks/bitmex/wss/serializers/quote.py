fe0490d3c75e9db328615982baf6c085e26773eb
Vitalii Myrhorodskyi <vmyrhorodskyi@gmail.com>
2021-09-10 14:55:17 +0300

    Fix Bitmex quote_bin WS

--- mst_gateway/connector/api/stocks/bitmex/wss/serializers/quote.py
+++ mst_gateway/connector/api/stocks/bitmex/wss/serializers/quote.py
@@ -15,6 +15,31 @@ class BitmexQuoteBinSerializer(BitmexSerializer):
     def __init__(self, wss_api: BitmexWssApi):
         super().__init__(wss_api)
         self._bins = {}
+        self._initialized = None
+
+    def prefetch(self, message: dict) -> None:
+        if message['table'] == 'tradeBin1m':
+            data = []
+            for item in message.pop('data', []):
+                if time := utils.to_date(item['timestamp']):
+                    item['timestamp'] = time.replace(minute=time.minute - 1, second=59, microsecond=999999)
+                data += [item, self.create_open_data(item)]
+            message['data'] = data
+
+    @staticmethod
+    def create_open_data(item: dict) -> dict:
+        if bin_time := utils.to_date(item.get('timestamp')):
+            bin_time = bin_time.replace(minute=bin_time.minute + 1, second=0, microsecond=000000)
+        close = item.get('close')
+        return {
+            'timestamp': utils.to_iso_datetime(bin_time),
+            'symbol': item.get('symbol'),
+            'open': close,
+            'close': close,
+            'high': close,
+            'low': close,
+            'volume': 0
+        }
 
     @classmethod
     def _get_data_action(cls, message) -> str:
@@ -23,7 +48,16 @@ class BitmexQuoteBinSerializer(BitmexSerializer):
         return 'update'
 
     def is_item_valid(self, message: dict, item: dict) -> bool:
-        return message['table'] in ('trade', 'tradeBin1m')
+        table = message['table']
+        if not self._initialized:
+            if table != 'tradeBin1m':
+                return False
+            self._initialized = utils.to_date(item.get('timestamp'))
+            return True
+        time = utils.to_date(item.get('timestamp'))
+        if time and time < self._initialized:
+            return False
+        return table in ('trade', 'tradeBin1m')
 
     async def _load_data(self, message: dict, item: dict) -> Optional[dict]:
         if not self.is_item_valid(message, item):
@@ -69,13 +103,7 @@ class BitmexQuoteBinSerializer(BitmexSerializer):
                 ts_new = datetime.strptime(item['timestamp'].split('Z')[0], api.DATETIME_FORMAT)
             except (ValueError, TypeError, IndexError):
                 return None
-            if (ts_new - ts_old).total_seconds() < 0:
+            if ts_new < ts_old:
                 return None
             return ts_new.minute > ts_old.minute
         return False
-
-    def prefetch(self, message: dict) -> None:
-        if message['table'] == 'tradeBin1m':
-            for d in message['data']:
-                if time := utils.to_date(d['timestamp']):
-                    d['timestamp'] = time.replace(minute=time.minute - 1, second=59, microsecond=999999)
