a4bc362855a1da45ebb0e754fc8a407e58db0b8f
Vitalii Myrhorodskyi <vmyrhorodskyi@gmail.com>
2021-10-26 12:39:36 +0300

    Fix FinFactory.calc_money_management()

--- mst_gateway/calculator/base.py
+++ mst_gateway/calculator/base.py
@@ -62,7 +62,9 @@ class FinFactory:
     def calc_money_management(cls, face_price: float, volume: float, balance: float,
                               commission: float) -> Tuple[float, Optional[float]]:
         try:
-            used = (face_price * volume * abs(commission))
+            used = face_price * volume
+            if commission:
+                used *= abs(commission)
             return used, used / balance
         except (TypeError, ZeroDivisionError):
             return 0, None
