8a87bdf99abb09a845d3e2345303847a3344fa51
Vitalii Myrhorodskyi <vmyrhorodskyi@gmail.com>
2021-12-09 15:34:18 +0200

    Rename env vars

--- tests/config/binance.py
+++ tests/config/binance.py
@@ -8,26 +8,26 @@ BINANCE_WSS_API_ACCOUNT_NAME = f"{BINANCE_ACCOUNT_NAME}.{BINANCE_ACCOUNT_ID}"
 BINANCE_SPOT_SCHEMA = OrderSchema.exchange
 BINANCE_FUTURES_SCHEMA = OrderSchema.futures
 BINANCE_SPOT_WSS_API_URL = getenv(
-    "BINANCE_SPOT_WSS_API_URL", "wss://stream.binance.com:9443/ws"
+    "GW_BINANCE_SPOT_WSS_API_URL", "wss://stream.binance.com:9443/ws"
 )
 BINANCE_FUTURES_WSS_API_URL = getenv(
-    "BINANCE_FUTURES_WSS_API_URL", "wss://fstream.binance.com/ws"
+    "GW_BINANCE_FUTURES_WSS_API_URL", "wss://fstream.binance.com/ws"
 )
 BINANCE_AUTH_KEYS = {
-    "api_key": getenv("BINANCE_API_KEY"),
-    "api_secret": getenv("BINANCE_API_SECRET"),
+    "api_key": getenv("GW_BINANCE_API_KEY"),
+    "api_secret": getenv("GW_BINANCE_API_SECRET"),
 }
 BINANCE_SPOT_TESTNET_WSS_API_URL = getenv(
-    "BINANCE_SPOT_TESTNET_WSS_API_URL", "wss://testnet.binance.vision/ws"
+    "GW_BINANCE_SPOT_TESTNET_WSS_API_URL", "wss://testnet.binance.vision/ws"
 )
 BINANCE_SPOT_TESTNET_AUTH_KEYS = {
     "api_key": getenv("BINANCE_SPOT_TESTNET_API_KEY"),
     "api_secret": getenv("BINANCE_SPOT_TESTNET_API_SECRET"),
 }
 BINANCE_FUTURES_TESTNET_WSS_API_URL = getenv(
-    "BINANCE_FUTURES_TESTNET_WSS_API_URL", "wss://stream.binancefuture.com/ws"
+    "GW_BINANCE_FUTURES_TESTNET_WSS_API_URL", "wss://stream.binancefuture.com/ws"
 )
 BINANCE_FUTURES_TESTNET_AUTH_KEYS = {
-    "api_key": getenv("BINANCE_FUTURES_TESTNET_API_KEY"),
-    "api_secret": getenv("BINANCE_FUTURES_TESTNET_API_SECRET"),
+    "api_key": getenv("GW_BINANCE_FUTURES_TESTNET_API_KEY"),
+    "api_secret": getenv("GW_BINANCE_FUTURES_TESTNET_API_SECRET"),
 }
