eeab04bf4aa3c83520a7cc6dd3a1146a92b1b0c9
Vitalii Myrhorodskyi <vmyrhorodskyi@gmail.com>
2021-12-09 19:34:13 +0200

    Rename env vars

--- tests/config/binance.py
+++ tests/config/binance.py
@@ -21,8 +21,8 @@ BINANCE_SPOT_TESTNET_WSS_API_URL = getenv(
     "GW_BINANCE_SPOT_TESTNET_WSS_API_URL", "wss://testnet.binance.vision/ws"
 )
 BINANCE_SPOT_TESTNET_AUTH_KEYS = {
-    "api_key": getenv("BINANCE_SPOT_TESTNET_API_KEY"),
-    "api_secret": getenv("BINANCE_SPOT_TESTNET_API_SECRET"),
+    "api_key": getenv("GW_BINANCE_SPOT_TESTNET_API_KEY"),
+    "api_secret": getenv("GW_BINANCE_SPOT_TESTNET_API_SECRET"),
 }
 BINANCE_FUTURES_TESTNET_WSS_API_URL = getenv(
     "GW_BINANCE_FUTURES_TESTNET_WSS_API_URL", "wss://stream.binancefuture.com/ws"
